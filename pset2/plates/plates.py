48590ee953a33a77ba7beb3d6ab53c1a65225888
Sidharth Jain <imsidharthj@gmail.com>
2024-04-20 15:35:05 +0530

    changes made in string loop elimination

--- pset2/plates/plates.py
+++ pset2/plates/plates.py
@@ -6,18 +6,22 @@ def main():
         print("Invalid")
 
 def is_valid(s):
-    # user = input{"""Enter the valid-plate that you would like. 
-    # Please keep it between 2 and 6 characters(letters or numbers). 
-    # Do not use periods, spaces, or punctuation in your vanity-plate
-    # letters at start and numbers at the end.(The first number used cannot be a ‘0’.)}
-    half_length = len(s) // 2
-    first_half = s[:half_length]
-    second_half = s[half_length:]
-    if (1 <= len(first_half) <= 3 and first_half.isalpha() and first_half.isupper()) and (
-        1 <= len(second_half) <= 3 and second_half.isnumeric()) and (
-        second_half[0] != '0'):
-        return True
-    else:
+    if (len(s) < 2) or (len(s) > 6):
         return False
-    
-main()
+    if not s[:2].isalpha():
+        return False
+    count_numbers = 0
+    for i in s[2:]:
+        if i.isdigit():
+            count_numbers = count_numbers + 1
+        if count_numbers == 1 and i == '0':
+            return False
+        if count_numbers >= 1 and (not i.isdigit()):
+            return False
+        if (not i.isalpha()) and (not i.isdigit()):
+            return False 
+
+    return True
+
+if __name__ == "__main__":
+    main()
