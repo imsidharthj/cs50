baf1126e0564794576dfd1c9e4a506739e506b52
Sidharth Jain <imsidharthj@gmail.com>
2024-04-20 15:39:43 +0530

    added define function and .strip()

--- pset1/bank/bank.py
+++ pset1/bank/bank.py
@@ -1,8 +1,10 @@
-greeting = input("hello ")
+def main():
+    greeting = input("hello ").strip().lower()
 
-if greeting.startswith("hello"):
-    print("$0")
-elif greeting.startswith("h"):
-    print("$20")
-else:
-    print("$100")
+    if greeting.startswith("hello"):
+        print("$0")
+    elif greeting.startswith("h"):
+        print("$20")
+    else:
+        print("$100")
+main()
