a6a1aaba3a5b1774950315f9559da5080a38217f
Sidharth Jain <imsidharthj@gmail.com>
2024-04-20 15:42:21 +0530

    added main function and .strip().lower()

--- pset1/deep/deep.py
+++ pset1/deep/deep.py
@@ -1,10 +1,13 @@
-a = input("What is the Answer to the Great Question of Life, the Universe and Everything ")
+def main():
 
-if a == "42":
-    print("yes")
-elif a == "forty two":
-    print("yes")
-elif a == "forty-two":
-    print("yes")  
-else:
-    print("no")
+    a = input("What is the Answer to the Great Question of Life, the Universe and Everything ").strip().lower()
+
+    if a == "42":
+        print("yes")
+    elif a == "forty-two":
+        print("yes")
+    elif a == "forty two":
+        print("yes")
+    else:
+        print("no")
+main()
