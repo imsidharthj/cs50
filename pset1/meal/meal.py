5b76031054cae03eeb8629b1b1674067db79646c
Sidharth Jain <imsidharthj@gmail.com>
2024-04-20 15:45:53 +0530

    changes made in conditions

--- pset1/meal/meal.py
+++ pset1/meal/meal.py
@@ -1,20 +1,20 @@
 def main():
-    time = input("what time it is? ")
-    timehours = convert(time)
+   time = input("what time it is? ")
+   timehours = convert(time)
 
-    if 7 <= timehours < 8:
-       print("breakfast time")
-    elif 12 <= timehours < 13:
-       print("lunch time")
-    elif 18 <= timehours < 19:
-       print("dinner time")
+   if 7 <= timehours < 8:
+      print("breakfast time")
+   elif 12 <= timehours <= 13:
+      print("lunch time")
+   elif 18 <= timehours < 19:
+      print("dinner time")
 
 def convert(time):
-    hours, minutes = time.split(":")
-    hours = float(hours)
-    minutes = float(minutes)
-    timehours = ((hours * 60) + minutes) / 60
-    return timehours
+   hours, minutes = time.split(":")
+   hours = float(hours)
+   minutes = float(minutes)
+   timehours = ((hours * 60) + minutes) / 60
+   return timehours
 
 if __name__ == "__main__":
-    main()
+   main()
